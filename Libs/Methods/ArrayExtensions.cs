d84b95ffbd2915a10e31a51c0d6d9fcae2461fb5
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2021-03-09 23:31:30 +0700

    Added Libs Catalog anf first lib - Methods

---
+++ Libs/Methods/ArrayExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Methods
+{
+    public static class ArrayExtensions
+    {
+        public static bool Swap3(this int[] array, int i1, int i2)
+        {
+            var temp = array[i1];
+            array[i1] = array[i2];
+            array[i2] = temp;
+
+            return true;
+        }
+    }
+}
