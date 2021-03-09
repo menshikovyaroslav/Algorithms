f7e59d1051d77df8fe629ac1ae62570ad1c0899c
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2021-03-10 00:20:42 +0700

    PowerAInB project

---
+++ PowerAInB/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PowerAInB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A");
+            var aStr = Console.ReadLine();
+            var a = int.Parse(aStr);
+
+            Console.WriteLine("Enter B");
+            var bStr = Console.ReadLine();
+            var b = int.Parse(bStr);
+
+            var result = 1;
+
+            while (b > 0)
+            {
+                result *= a;
+                b--;
+            }
+
+            Console.WriteLine($"Result = {result}");
+            Console.ReadKey();
+        }
+    }
+}
