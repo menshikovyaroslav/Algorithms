6068e168f7a1a4a6e3600b35bba54d7bbe20d72f
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2021-03-09 23:54:01 +0700

    Added Reverse Num project

---
+++ ReverseNum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ReverseNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = 123;
+            var result = 0;
+
+            while (num > 0)
+            {
+                result = result * 10 + num % 10;
+                num /= 10;
+            }
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
