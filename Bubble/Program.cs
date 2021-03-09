1a26f407e805a33f71fe494034de5c2123b79b9c
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2021-03-09 23:36:19 +0700

    Added Bubble Sort

---
+++ Bubble/Program.cs
@@ -0,0 +1,35 @@
+﻿using Methods;
+using System;
+
+namespace Bubble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[10] { 3, 5, 8, 0, 1, 9, 7, 2, 4, 6 };
+
+            while (true)
+            {
+                bool wasSwaps = false;
+                for (int i = 1; i < array.Length; i++)
+                {
+                    if (array[i] < array[i - 1])
+                    {
+                        array.Swap3(i, i - 1);
+                        wasSwaps = true;
+                    }
+                }
+
+                if (!wasSwaps) break;
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
