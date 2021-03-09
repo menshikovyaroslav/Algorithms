5f5790152c7b161a7e5b8c23f114edba13ed68fa
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2021-03-09 23:40:03 +0700

    Added swap without using a temp var

--- Libs/Methods/ArrayExtensions.cs
+++ Libs/Methods/ArrayExtensions.cs
@@ -4,6 +4,13 @@ namespace Methods
 {
     public static class ArrayExtensions
     {
+        public static void Swap2(this int[] array, int i1, int i2)
+        {
+            array[i1] *= array[i2];
+            array[i2] = array[i1] / array[i2];
+            array[i1] /= array[i2];
+        }
+
         public static bool Swap3(this int[] array, int i1, int i2)
         {
             var temp = array[i1];
