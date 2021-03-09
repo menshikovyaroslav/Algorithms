9a924d0cdb9b63f31aae29702cb7246fd70b89c0
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2021-03-09 23:51:04 +0700

    Added project Sum of digits

---
+++ SumDigits/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = 123;
+            var result = 0;
+
+            while (num > 0)
+            {
+                result += num % 10;
+                num /= 10;
+            }
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
