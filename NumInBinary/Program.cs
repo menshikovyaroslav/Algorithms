932b3d173c150350b0f0943a5b890c0fa80838e6
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2021-03-10 00:00:58 +0700

    Num in Binary without recursion

---
+++ NumInBinary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NumInBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = 23;
+            var result = "";
+
+            while (num > 0)
+            {
+                result += num % 2;
+                num /= 2;
+            }
+
+            for (int i = result.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{result[i]}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
