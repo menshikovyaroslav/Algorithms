2324972a576dc10ba64eabe7192a89b1b03a63af
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2021-03-12 15:39:23 +0700

    End of algorithm

--- AlgorithmLee/MainWindow.xaml.cs
+++ AlgorithmLee/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,11 +28,13 @@ namespace AlgorithmLee
 
         private void StartBtn_Click(object sender, RoutedEventArgs e)
         {
+            #region Map Bounds
+
             int mapH, mapW = 20;
             Int32.TryParse(MapHeight.Text, out mapH);
             Int32.TryParse(MapWidth.Text, out mapW);
 
-            var map = new int[mapH, mapW];
+            var map = new int?[mapH, mapW];
             var blockCount = (mapH - 2) * (mapW - 2) * 25 / 100;
 
             // Left and right labirithm bounds
@@ -53,24 +56,28 @@ namespace AlgorithmLee
             {
                 var rndH = rnd.Next(1, mapH - 1);
                 var rndW = rnd.Next(1, mapW - 1);
-                if (map[rndH, rndW] == 0)
+                if (map[rndH, rndW] == null)
                 {
                     map[rndH, rndW] = -1;
                     blockCount--;
                 }
             }
 
-            var aField = new Field(0, 0, 1);
-            var bField = new Field(0, 0, 1);
+            #endregion
+
+            #region Creating A and B points
+
+            Field aField = null;
+            Field bField = null;
 
             while (true)
             {
                 var h = rnd.Next(1, mapH - 1);
                 var w = rnd.Next(1, mapW - 1);
 
-                if (map[h, w] == 0)
+                if (map[h, w] == null)
                 {
-                    aField = new Field(h, w, 1);
+                    aField = new Field(h, w, 0);
                     break;
                 }
             }
@@ -80,14 +87,16 @@ namespace AlgorithmLee
                 var h = rnd.Next(1, mapH - 1);
                 var w = rnd.Next(1, mapW - 1);
 
-                if (map[h, w] == 0 && !(aField.I == h && aField.J == w))
+                if (map[h, w] == null && !(h == aField.I && w == aField.J))
                 {
-                    bField = new Field(h, w, 1);
+                    bField = new Field(h, w, null);
                     break;
                 }
             }
 
-            map[aField.I, aField.J] = 1;
+            #endregion
+
+            #region Filling the map
 
             var queue = new Queue<Field>();
             queue.Enqueue(aField);
@@ -96,15 +105,77 @@ namespace AlgorithmLee
             {
                 var field = queue.Dequeue();
 
-                if (map[field.I - 1, field.J] == 0) queue.Enqueue(new Field(field.I - 1, field.J, field.V + 1));
-                if (map[field.I, field.J + 1] == 0) queue.Enqueue(new Field(field.I, field.J + 1, field.V + 1));
-                if (map[field.I + 1, field.J] == 0) queue.Enqueue(new Field(field.I + 1, field.J, field.V + 1));
-                if (map[field.I, field.J - 1] == 0) queue.Enqueue(new Field(field.I, field.J - 1, field.V + 1));
+                if (map[field.I, field.J] > 0) continue;
 
-                if (map[field.I, field.J] == 0) map[field.I, field.J] = field.V;
+                if (map[field.I - 1, field.J] == null) queue.Enqueue(new Field(field.I - 1, field.J, field.V + 1));
+                if (map[field.I, field.J + 1] == null) queue.Enqueue(new Field(field.I, field.J + 1, field.V + 1));
+                if (map[field.I + 1, field.J] == null) queue.Enqueue(new Field(field.I + 1, field.J, field.V + 1));
+                if (map[field.I, field.J - 1] == null) queue.Enqueue(new Field(field.I, field.J - 1, field.V + 1));
 
-                if (field.I == bField.I && field.J == bField.J) break;
+                if (map[field.I, field.J] == null) map[field.I, field.J] = field.V;
+
+                if (field.Equals(bField))
+                {
+                    bField.V = field.V;
+                    break;
+                }
             }
+
+            queue.Clear();
+
+            #endregion
+
+            #region Finding the right shortest path
+
+            var list = new List<Field>();
+
+            if (bField.V == null)
+            {
+                MessageBox.Show("There is now path from A to B");
+            }
+            else
+            {
+                queue.Enqueue(bField);
+                while (true)
+                {
+                    var field = queue.Dequeue();
+
+                    list.Add(field);
+
+                    if (field.V == 0) break;
+
+                    if (map[field.I - 1, field.J] == field.V - 1)
+                    {
+                        queue.Enqueue(new Field(field.I - 1, field.J, field.V - 1));
+                        continue;
+                    }
+
+                    if (map[field.I, field.J + 1] == field.V - 1)
+                    {
+                        queue.Enqueue(new Field(field.I, field.J + 1, field.V - 1));
+                        continue;
+                    }
+
+                    if (map[field.I + 1, field.J] == field.V - 1)
+                    {
+                        queue.Enqueue(new Field(field.I + 1, field.J, field.V - 1));
+                        continue;
+                    }
+
+                    if (map[field.I, field.J - 1] == field.V - 1)
+                    {
+                        queue.Enqueue(new Field(field.I, field.J - 1, field.V - 1));
+                        continue;
+                    }
+
+                }
+            }
+
+            #endregion
+
+            #region Show map on the main window
+
+            Map.Children.Clear();
 
             for (int i = 0; i < mapH; i++)
             {
@@ -134,29 +205,37 @@ namespace AlgorithmLee
                         rect.Fill = Brushes.White;
                     }
 
-                    if (map[i, j] > 0)
+                    if (map[i, j] >= 0)
                     {
                         Map.Children.Add(label);
                         Canvas.SetTop(label, i * 20);
                         Canvas.SetLeft(label, j * 20);
                     }
 
-                    if (i == bField.I && j == bField.J)
+                    var field = list.SingleOrDefault(el => el.I == i && el.J == j);
+
+                    if (field != null)
                     {
                         label.Foreground = Brushes.Green;
                         label.FontWeight = FontWeights.Bold;
+
+                        if (field.Equals(bField)) label.Content = "B";
                     }
+
+
                 }
             }
+
+            #endregion
 
         }
     }
 
-    public class Field
+    public class Field : IEquatable<Field>
     {
         private int _i;
         private int _j;
-        private int _v;
+        private int? _v;
 
         public int I
         {
@@ -170,17 +249,22 @@ namespace AlgorithmLee
             set { _j = value; }
         }
 
-        public int V
+        public int? V
         {
             get { return _v; }
             set { _v = value; }
         }
 
-        public Field(int i, int j, int v)
+        public Field(int i, int j, int? v)
         {
             I = i;
             J = j;
             V = v;
+        }
+
+        public bool Equals([AllowNull] Field other)
+        {
+            return I == other.I && J == other.J;
         }
     }
 }
