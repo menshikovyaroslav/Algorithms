0ef7596e0a1fadcc380d7b6d9d4053c2f4dc4c9b
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2021-03-15 01:10:01 +0700

    SearchStringInString project

---
+++ SearchStringInString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SearchStringInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s1 = "wor";
+            var s2 = "hellwor wodld";
+
+            int i, j;
+
+            var m = s1.Length;
+            var n = s2.Length;
+
+            for (i = 0; i < n - m; i++)
+            {
+                j = 0;
+                while (j < m && s2[i + j] == s1[j])
+                    j++;
+                if (j == m) break;
+            }
+        }
+    }
+}
